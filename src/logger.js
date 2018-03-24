8d1278be898d2c2fdf8a9e3f47c9fa9a34dfc418
Michael Perrotte <michael.perrotte@resolver.com>
2018-03-23 23:26:35 -0400

    added logger

---
+++ src/logger.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const Winston = require('winston');
+
+const DefaultTransport = new Winston.transports.Console({
+  colorize: true,
+  prettyPrint: true,
+  depth: 3
+});
+
+exports.getLogger = (level) => {
+  if (!level) throw new Error('invalid.function.call');
+
+  return new Winston.Logger({
+    level,
+    transports: [ DefaultTransport ]
+  });
+};
