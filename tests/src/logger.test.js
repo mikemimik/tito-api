8623a5be9e0c9e8390934aa67bfb915537919bd8
Michael Perrotte <michael.perrotte@resolver.com>
2018-03-26 02:35:53 -0400

    add tests for logger file

---
+++ tests/src/logger.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const test = require('tape');
+
+const logger = require('../../src/logger');
+
+test('Logger', (t) => {
+  t.throws(
+    logger.getLogger,
+    /invalid.function.call/,
+    'should throw if no log level is provided'
+  );
+
+  const a = logger.getLogger('debug');
+  t.equals(
+    typeof a.debug,
+    'function',
+    'should have debug function if level was set to debug'
+  );
+
+  t.equals(
+    typeof a.error,
+    'function',
+    'should have error function if level was set to debug'
+  );
+
+  t.equals(
+    typeof a.silly,
+    'function',
+    'should have silly function if level was set to debug'
+  );
+  t.end();
+});
