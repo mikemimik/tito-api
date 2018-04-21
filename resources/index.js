d994aa124a18bced49065537d798045d3220333a
Michael Perrotte <michael.perrotte@resolver.com>
2018-04-20 23:51:36 -0400

    added resources index file

---
+++ resources/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Activity = require('./activity');
+const CheckIn = require('./checkin');
+const CheckinList = require('./checkinList');
+const Event = require('./event');
+const Release = require('./release');
+const Ticket = require('./ticket');
+
+exports.Activity = Activity;
+exports.CheckIn = CheckIn;
+exports.CheckinList = CheckinList;
+exports.Event = Event;
+exports.Release = Release;
+exports.Ticket = Ticket;
