e9071aa841b72aea8c6ce8cff35925c3c4d23215
Michael Perrotte <michael.perrotte@resolver.com>
2018-04-21 01:30:10 -0400

    updated checkin resource

--- resources/checkin.js
+++ resources/checkin.js
@@ -1,8 +1,28 @@
 'use strict';
 
 class Checkin {
-  constructor (options) {
-    this.createdAt = ''; // DATETIME
+  constructor (options, fromSelf = false) {
+    const {
+      id,
+      type,
+      createdAt
+    } = options;
+    this.id = id || null;
+    this.type = type || 'checkins';
+    this.createdAt = (fromSelf)
+      ? createdAt
+      : new Date(); // DATETIME
+  }
+
+  static from (incoming) {
+    // const { id, type, attributes, links, relationships } = incoming;
+    const { id, type, attributes } = incoming;
+    const options = {
+      id,
+      type,
+      createdAt: attributes['created-at']
+    };
+    return new Checkin(options, true);
   }
 }
 
