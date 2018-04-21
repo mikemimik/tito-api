1e1d381884d222564a93fd00c19c2bd3a4943a7e
Michael Perrotte <michael.perrotte@resolver.com>
2018-04-21 15:28:25 -0400

    updated ticket resource with from method

--- resources/ticket.js
+++ resources/ticket.js
@@ -9,19 +9,77 @@ const TICKET_STATE = {
 };
 
 class Ticket {
-  constructor (options) {
-    this.answers = {}; // HASH
-    this.companyName = ''; // STRING
-    this.email = ''; // STRING
-    this.name = ''; // STRING
-    this.number = 1; // INTEGER
-    this.phoneNumber = ''; // STRING
-    this.price = 0.0; // DECIMAL
-    this.reference = ''; // STRING
-    this.state = TICKET_STATE.NEW; // ENUM
-    this.tags = []; // STRING[]
-    this.temporary = false; // BOOLEAN
-    this.testMode = false; // BOOLEAN
+  constructor (options, fromSelf = false) {
+    const {
+      id,
+      type,
+      answers,
+      companyName,
+      email,
+      name,
+      number,
+      phoneNumber,
+      price,
+      reference,
+      state,
+      tags,
+      temporary,
+      testMode
+    } = options;
+    this.answers = answers || {}; // HASH
+    this.companyName = companyName || ''; // STRING
+    if (!fromSelf && !email) {
+      throw new Error('missing.option.EMAIL');
+    }
+    this.email = email || 'no-email'; // STRING - REQUIRED
+    if (!fromSelf && !name) {
+      throw new Error('missing.option.NAME');
+    }
+    this.name = name || 'no-name'; // STRING - REQUIRED
+    if (!fromSelf && !number) {
+      throw new Error('missing.option.NUMBER');
+    }
+    this.number = number; // INTEGER
+    this.phoneNumber = phoneNumber || ''; // STRING
+    this.price = price || 0.0; // DECIMAL
+    this.reference = reference || ''; // STRING
+    this.state = TICKET_STATE[state] || TICKET_STATE.NEW; // ENUM - REQUIRED
+    this.tags = tags || []; // STRING[]
+    this.temporary = temporary || false; // BOOLEAN
+    this.testMode = testMode || false; // BOOLEAN
+  }
+
+  static from (incoming) {
+    // const { id, type, attributes, links, relationships } = incoming;
+    const { id, type, attributes } = incoming;
+    const {
+      answers,
+      email,
+      name,
+      number,
+      price,
+      reference,
+      state,
+      tags,
+      temporary
+    } = attributes;
+    const options = {
+      id,
+      type,
+      answers,
+      companyName: attributes['company-name'],
+      email,
+      name,
+      number,
+      phoneNumber: attributes['phone-number'],
+      price,
+      reference,
+      state,
+      tags,
+      temporary,
+      testMode: attributes['test-mode']
+    };
+    return new Ticket(options, true);
   }
 }
 
